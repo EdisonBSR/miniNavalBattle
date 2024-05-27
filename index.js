40e4dc8615db89c78cf7e9434a102936a902d924
EdisonBSR <edisobladimir.solanor@gmail.com>
2024-05-26 23:21:24 -0500

    add eventListener boat_position y attack

--- index.js
+++ index.js
@@ -3,25 +3,35 @@ import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
-import { emit } from "node:process";
+import { log } from "node:console";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
+let player1 = "";
+let player2 = "";
+let board1 = [];
+let board2 = [];
+const ID_BOAT = "bs";
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
 io.on("connection", (socket) => {
-  let player1 = "";
-  let player2 = "";
-  let board1 = [];
-  let board2 = [];
-  const ID_BOAT = "bs";
-  // emit.io("game", player1, player2);
+  socket.on("load player", (user) => {
+    console, log(user);
+    if (player1 == "") {
+      player1 = user;
+      io.emit("load player", player1);
+    } else if (player2 == "") {
+      player2 = user;
+      io.emit("load player", user);
+    } else {
+      console.log(`Jugador 1 ${player1} - Jugador 2 ${player2}`);
+    }
+  });
+  //VOLVER A ANALIZAR, EN BASE A LOS CAMBIOS REALIZADOS HTML Y NOMBRADO DE COMUNICACION SOCKET.
   socket.on("start battle", (user, boardPlayer) => {
     if (player1 == "") {
       player1 = user;
@@ -39,11 +49,12 @@ io.on("connection", (socket) => {
       );
     }
   });
-  socket.on("game", (user, row, column) => {
+  //ELIMINAR BUTTON POR QUE LA IDEA ES MANEJAR FILA Y COLUMNA CON UN 1 AL INICIO.
+  socket.on("game", (user, row, column, BUTTON) => {
     if (player1 == user) {
       let ponit = 1;
       if (board2[row][column] == ID_BOAT) {
-        io.emit("response attack", player1, ponit, row, column);
+        io.emit("response attack", player1, ponit, row, column, BUTTON);
       } else {
         ponit = 0;
         io.emit("response attack", player1, ponit, row, column);
